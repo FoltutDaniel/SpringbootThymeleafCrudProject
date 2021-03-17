91f8d545a59a69d05eac6186b6c9aac915b5e69b
Foltut Daniel <danfoltut@gmail.com>
2021-03-17 21:28:33 +0200

    Spring initializer project

---
+++ src/main/java/com/zeke/spring/ThymeleafEmployees/ThymeleafEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.zeke.spring.ThymeleafEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeke/spring/ThymeleafEmployees/ThymeleafEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeke.spring.ThymeleafEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
