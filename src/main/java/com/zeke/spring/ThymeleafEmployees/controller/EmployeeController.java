3617da0da9585f208306a280b60f8a89842d6eef
Foltut Daniel <danfoltut@gmail.com>
2021-03-19 21:10:01 +0200

    All CRUD functionality implemented

--- src/main/java/com/zeke/spring/ThymeleafEmployees/controller/EmployeeController.java
+++ src/main/java/com/zeke/spring/ThymeleafEmployees/controller/EmployeeController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.zeke.spring.ThymeleafEmployees.entity.Employee;
 import com.zeke.spring.ThymeleafEmployees.service.EmployeeService;
@@ -39,8 +41,28 @@ public class EmployeeController {
 	}
 	
 	@PostMapping("/save")
-	public String addEmployee() {
-		return null;
+	public String addEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		employeeService.saveEmployee(employee);
+		
+		return "redirect:/employees/list";
+	}
+	
+	@GetMapping("showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+		
+		//get employee from service
+		Employee theEmployee = employeeService.getEmployee(theId);
+		theModel.addAttribute("employee",theEmployee);
+		
+		return "add-form";
+	}
+	
+	@GetMapping("/delete")
+	public String  delete(@RequestParam("employeeId") int theId) {
+		
+		employeeService.deleteEmployee(theId);
+		return "redirect:/employees/list";
 	}
 }
 
